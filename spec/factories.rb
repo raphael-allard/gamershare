4eedf933ca0b5306fbf63d0336cd55f9b42cae27
raphael allard <raphael.s.allard@gmail.com>
2021-07-21 11:37:22 +0200

    added some validations tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
-    email {"test@gmail.com"}
-    password {"azerty"}
+    email { "test@gmail.com" }
+    password { "azerty" }
+    username { "test" }
+    address { "5 rue erard, Paris"}
   end
 end
