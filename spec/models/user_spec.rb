43d91f46903d1426b565cb8f4ca939ec0808d0ef
raphael allard <raphael.s.allard@gmail.com>
2021-07-21 12:35:51 +0200

    user model validations done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
+  let!(:user) { create(:user) }
 
-  describe '#new' do
+  describe '::new' do
+    context 'persistence' do
+      it 'persists user' do
+        expect(User.count).to eq(1)
+      end
+    end
+
     context 'validations' do
       it 'is valid with valid attributes' do
-        expect(create(:user)).to be_valid
+        expect(user).to be_valid
       end
 
       it 'is not valid without username' do
         user.username = nil
         expect(user).to_not be_valid
+      end
+
+      it 'is not valid with username already taken' do
+        new_user = build(:user)
+        new_user.email = "new_test@gmail.com"
+        new_user.valid?
+        expect(new_user.errors.messages).to include(:username)
       end
 
       it 'is not valid without address' do
