507dc43bd4f7262a91abacf390fd4f4fb7fa410b
raphael allard <raphael.s.allard@gmail.com>
2021-07-23 19:46:36 +0200

    added call api service from igdb + factory game

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,15 @@ FactoryBot.define do
     username { "test" }
     city { "Paris"}
   end
+
+  factory :game do
+    title { "Mega Man X" }
+    nostalgia_point { 9 }
+    console { "Super Nintendo" }
+    developer { "Capcom" }
+    release_date { Date.new(1994,1,1) }
+    game_mode { "Single player" }
+    genre { ["Adventure", "Platform", "Shooter"] }
+    association :owner, factory: :user
+  end
 end
