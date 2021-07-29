7db6725f9428266e7d5050244fef1039dfb4126a
raphael allard <raphael.s.allard@gmail.com>
2021-07-29 10:17:56 +0200

    trade associations done

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,4 +1,5 @@
 class Trade < ApplicationRecord
   belongs_to :asker, class_name: "User"
   belongs_to :receiver, class_name: "User"
+  has_many :trade_games, dependent: :destroy
 end
--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -9,5 +9,17 @@ RSpec.describe Trade, type: :model do
         expect(Trade.count).to eq(1)
       end
     end
+
+    context 'associations' do
+      it 'has many trade games' do
+        expect(trade.trade_games.new.class).to eq(TradeGame)
+      end
+
+      it 'destroys trade games associated' do
+        trade.trade_games.create(game: build(:game))
+        trade.destroy
+        expect(trade.trade_games.count).to eq(0)
+      end
+    end
   end
 end
