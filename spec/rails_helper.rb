a3546a2698262a809afb4481931b90d947437ece
raphael allard <raphael.s.allard@gmail.com>
2021-08-02 20:23:46 +0200

    refacto specs + removed some useless lines of code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -63,4 +63,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Use Devise helpers in tests
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  # config.include RequestSpecHelper, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,9 +1,3 @@
 RSpec.configure do |config|
-  config.include Warden::Test::Helpers
-
-  config.after :each do
-    Warden.test_reset!
-  end
-
   config.include FactoryBot::Syntax::Methods
 end
