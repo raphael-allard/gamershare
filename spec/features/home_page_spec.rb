0c787da05c0a9987537c23a2708fee8b49da6216
raphael allard <raphael.s.allard@gmail.com>
2021-08-02 16:51:15 +0200

    added some main setup to rspec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature "Home page", type: :feature do
-  scenario "user visites the home page" do
+  scenario "user accesses the content of home page" do
     visit root_path
-    expect(page).to have_content "lol"
+    expect(page).to have_content "pages"
   end
 end
