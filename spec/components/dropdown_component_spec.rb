e0d44009f211efdff6208a334f53506131d7db2c
raphael allard <raphael.s.allard@gmail.com>
2021-08-09 09:47:31 +0200

    improved main design of website

--- spec/components/dropdown_component_spec.rb
+++ spec/components/dropdown_component_spec.rb
@@ -1,13 +1,9 @@
 require "rails_helper"
 
 RSpec.describe DropdownComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders component" do
+    render_inline(described_class.new(user: build(:user)))
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+    expect(rendered_component).to have_text "Log out"
+  end
 end
