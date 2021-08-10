1e64a776c256a8dacb9fb1838ead40ac8acd5e8c
raphael allard <raphael.s.allard@gmail.com>
2021-08-10 09:45:15 +0200

    added nested form for trade & trade_game

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,5 +2,9 @@ class TradesController < ApplicationController
   def new
     @trade = Trade.new
     authorize @trade
+    
+    @trade.trade_games.new
+    @asker = User.find(params[:asker])
+    @receiver = User.find(params[:receiver])
   end
 end
