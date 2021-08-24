84aaba1cd0d5cd49d8c29b7f42a4dfefa2e8be68
raphael allard <raphael.s.allard@gmail.com>
2021-08-24 12:14:32 +0200

    added some options to screenshots

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,7 @@
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.save_path = Rails.root.join('tmp/capybara')
+Capybara.javascript_driver = :headless_chrome
